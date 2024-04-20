9044de686343515312cb317d38581eae0eddfda1
KrishPatel163 <krishhpatel7587@gmail.com>
2024-04-20 21:49:11 +0530

    set up the routes and controllers

--- src/app.js
+++ src/app.js
@@ -2,23 +2,17 @@ import express from "express";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-
 const app = express()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
-
-app.use(express.json({
-    limit: "20kb"
-}))
-
-app.use(express.urlencoded({
-    extended: true,
-    limit: "20kb"
-}))
-
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
+app.use(express.json({ limit: "20kb" }))
+app.use(express.urlencoded({ extended: true, limit: "20kb" }))
 app.use(cookieParser())
+
+// Import Routes
+import userRouter from "./routes/user.routes.js"
+
+//Declare Routes
+app.use("/api/v1/users", userRouter)
 
 export default app
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registeUser = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: "ok"
+    })
+})
+
+export {
+    registeUser,
+}
